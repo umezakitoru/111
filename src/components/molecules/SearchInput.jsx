a584dd5a5ae57201e530081df83c20db02d7b0a6
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-01-22 19:04:15 +0900

    111

---
+++ src/components/molecules/SearchInput.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { Input } from "../atoms/input/input";
+
+export const SearchInput = () => {
+  return (
+    <div>
+      <SContainer>
+        <Input placeholder="あああ" />
+        <SButtonWrapper>
+          <PrimaryButton>検索</PrimaryButton>
+        </SButtonWrapper>
+      </SContainer>
+    </div>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const SButtonWrapper = styled.div`
+  padding-left: 8px;
+`;
