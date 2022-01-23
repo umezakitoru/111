1e5933a9cb410e123159dd3c9d7d1938a969396f
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-01-23 17:08:02 +0900

    111

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>aaa.aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>00-000-0000</dd>
+        <dt>会社名</dt>
+        <dd>ヴァリアント</dd>
+        <dt>WEB</dt>
+        <dd>123.com</dd>
+      </dl>
+    </div>
+  );
+};
