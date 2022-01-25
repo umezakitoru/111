0a918e0a3bab617de02819b3f3ad5b883bed0166
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-01-26 07:21:39 +0900

    111

---
+++ src/components/pages/Top.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Top = () => {
+  return (
+    <SContaner>
+      <h2>TOPページです</h2>
+    </SContaner>
+  );
+};
+
+const SContaner = styled.div`
+  text-align: center;
+`;
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Users = () => {
+  return (
+    <SContaner>
+      <h2>ユーザー一覧</h2>
+    </SContaner>
+  );
+};
+
+const SContaner = styled.div`
+  text-align: center;
+`;
