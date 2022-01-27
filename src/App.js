60a8fbb69deffdb92650f21337744c87175bf627
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-01-28 07:15:07 +0900

    111

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import React from "react";
+import { UserProvider } from "./provider/UserProvider";
 import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
-  return <Router />;
+  return (
+    <UserProvider>
+      <Router />;
+    </UserProvider>
+  );
 }
