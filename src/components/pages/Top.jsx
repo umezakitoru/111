0619fae29c3b33e143770246714e94ab6ddbcb09
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-01-29 14:53:46 +0900

    1111

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,13 +1,20 @@
 import styled from "styled-components";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
 import { useHistory } from "react-router-dom";
+import { useContext } from "react";
 
 export const Top = () => {
   const history = useHistory();
-  const onClickAdmin = () =>
-    history.push({ pathname: "/users", state: { isAdmin: true } });
-  const onClickGeneral = () =>
-    history.push({ pathname: "/users", state: { isAdmin: false } });
+  const [setUserInfo] = useContext();
+
+  const onClickAdmin = () => {
+    setUserInfo({ isAdmin: true });
+    history.push("/users");
+  };
+  const onClickGeneral = () => {
+    setUserInfo({ isAdmin: false });
+    history.push("/users");
+  };
   return (
     <SContainer>
       <h2>TOPページ</h2>
